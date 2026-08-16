bdc4b736fc338bbddf44f7788fefe350588e2e82
adityarajahuja8 <adityarajahuja8@gmail.com>
2026-08-16 22:49:11 +0530

    LC-next-greater-element-ii [Runtime: N/A; Memory: N/A]

---
+++ array/next-greater-element-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i=n*2-1; i>=0; i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
+            }
+            
+            if(st.empty() &&i<n) ans[i%n]=-1;
+
+            else if(i<n){
+                ans[i%n]=st.top();
+            }
+
+            st.push(nums[i%n]);
+
+        }
+        return ans;
+    }
+};
