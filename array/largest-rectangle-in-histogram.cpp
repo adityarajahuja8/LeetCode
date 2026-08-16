aadd3995703af069647e329c3324d5cf948d5445
adityarajahuja8 <adityarajahuja8@gmail.com>
2026-08-16 22:38:18 +0530

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

---
+++ array/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& hi) {
+        stack<int> st;
+        // vector<int> ans;
+        int ma=0,left;
+        for(int i=0;i<hi.size();i++){
+            while(!st.empty() && hi[st.top()]>=hi[i]){
+                int popped=st.top();
+                st.pop();
+                if(st.empty()) left=-1;
+                else{
+                    left=st.top();
+                }
+                
+                int w=i-left-1;
+                ma=max(hi[popped]*w,ma);
+            }
+            st.push(i);
+
+            
+            // st.push(i);
+
+        }
+        while(!st.empty()){
+            int pop=st.top();
+            st.pop();
+            if(st.empty()) left=-1;
+            else{
+                left=st.top();
+            }
+            int w=hi.size()-left-1;
+            ma=max(hi[pop]*w,ma);
+            x++;
+        }
+        // ans=max(ma,mx);
+        return ma;
+    }
+};
