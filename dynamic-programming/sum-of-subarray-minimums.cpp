b5119d8017161fa493f4b6634bc9d96534e94d14
adityarajahuja8 <adityarajahuja8@gmail.com>
2026-08-17 15:45:30 +0530

    LC-sum-of-subarray-minimums [Runtime: N/A; Memory: N/A]

--- dynamic-programming/sum-of-subarray-minimums.cpp
+++ dynamic-programming/sum-of-subarray-minimums.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-    int n=arr.size();
+    int n=arr.size(),a=0,b=0;
+    long long ans=0;
     stack<int> st;
-    vector<int> ans;
+    vector<int> right(n),left(n);
+    // vector<int> left;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        
+        if(st.empty()) right[i]=n;
+        else{
+            right[i]=st.top();
+        }
+        
+        
+        st.push(i);
+    }
+
+    while(!st.empty()) st.pop();
     for(int i=0;i<n;i++){
         while(!st.empty() && arr[st.top()]>arr[i]){
             st.pop();
         }
-        if(st.empty()) ans[i]=-1;
+        
+        if(st.empty()) left[i]=-1;
         else{
-            ans[i]=st.top();
+            left[i]=st.top();
         }
+        
         st.push(i);
     }
+    long long cont;
+    long long MOD=1000000007;
     for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
+        int l=i-left[i];
+        int r=right[i]-i;
+        cont=1LL*arr[i]*l*r;
+        ans=(ans+cont)%MOD;
     }
-    return i;
+
+    return ans;
+    }
 };
