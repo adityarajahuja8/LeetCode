7b678b6983644b9a5a4919258846c8e61b17d4d0
adityarajahuja8 <adityarajahuja8@gmail.com>
2026-09-07 22:39:45 +0530

    LC-split-array-largest-sum [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/split-array-largest-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int splitArray(vector<int>& arr, int k) {
+        int n=arr.size();
+        int l=0,r=0;
+        for(int i=0;i<n;i++){
+            l=max(l,arr[i]);
+            r+=arr[i];
+        }
+
+        while(l<r){
+            int curr=0,sub=1;
+            int mid=l+(r-l)/2;
+            for(int i=0;i<n;i++){
+                if(curr+arr[i]<mid){
+                    curr+=arr[i];
+                }
+                else{
+                    sub++;
+                    curr=arr[i];
+                }
+            }
+            if(sub>k){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return r;
+    }
+};
